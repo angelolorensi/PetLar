aa2b9be493f5b8f689e7931762ecb35a8ce50017
angelo lorensi <angelomozza@gmail.com>
2024-01-10 13:12:34 -0300

    pet model

--- app/Models/Pet.php
+++ app/Models/Pet.php
@@ -9,5 +9,24 @@ class Pet extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'species',
+        'sex',
+        'size',
+        'age',
+        'neutered',
+        'vaccinated',
+        'dewormed',
+        'special_care',
+        'temperament',
+        'living_environment',
+        'socializes_with',
+        'description',
+    ];
 
+    public function images()
+    {
+        return $this->hasMany(PetImage::class);
+    }
 }
